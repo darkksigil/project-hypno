82a899d6f13d8109d8a8b215fc9f2d741492b1f8
miso-duty <miso-duty@vmmc.gov.ph>
2026-02-18 15:30:19 +0800

    fixed login viewport size

--- frontend/app/src/app/app.component.ts
+++ frontend/app/src/app/app.component.ts
@@ -1,13 +1,29 @@
 // src/app/app.component.ts
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {}
+
+export class AppComponent {
+  private router = inject(Router);
+
+  // True when current route is NOT login
+  showNavbar = toSignal(
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map(e => !(e as NavigationEnd).urlAfterRedirects.startsWith('/login'))
+    ),
+    { initialValue: false } // hide navbar on first load until route resolves
+  );
+}
